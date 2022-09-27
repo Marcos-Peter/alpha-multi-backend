f098dc2b8e4e380616c6b5ab55d2371f7c3b83bb
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-09-27 17:59:38 -0400

    retirado static

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,19 @@ import { websocketServer } from './websocket/websocketServer';
 dotenv.config();
 
 const app = express();
-const appAdmin = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/../alpha-multi-frontend/dist`));
-appAdmin.use(express.static(`${__dirname}/../alpha-multi-frontend-admin/dist`));
-
 routes.initRoutes(app);
 
 const port = process.env.PORT || 3000;
-const portAdmin = 3334;
 
 app.listen(port, () =>
 {
     console.log(`Alpha Multi Server is running at port ${port}`);
-});
-
-appAdmin.listen(portAdmin, () =>
-{
-    console.log(`Alpha Multi FrontEnd Admin is running at port ${portAdmin}`);
 });
 
 websocketServer.init();
