7ceb3aed4bcc88c00406e1fb4076e88f7842ef29
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-09-27 23:50:45 -0400

    s

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { websocketServer } from './websocket/websocketServer';
 dotenv.config();
 
 const app = express();
+const appAdmin = express();
 
 const privateKey  = fs.readFileSync('certificate.key', 'utf8');
 const certificate = fs.readFileSync('certificate.crt', 'utf8');
@@ -21,18 +22,30 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+appAdmin.use(cors({ origin: true, credentials: true }));
+appAdmin.use(cookieParser());
+appAdmin.use(express.urlencoded({ extended: false }));
+appAdmin.use(express.json());
+
 app.use(express.static('/root/osArrematadores/alpha-multi-frontend/dist'));
-app.use(express.static('/root/osArrematadores/alpha-multi-frontend-admin/dist'));
+appAdmin.use(express.static('/root/osArrematadores/alpha-multi-frontend-admin/dist'));
 
 routes.initRoutes(app);
+routes.initRoutes(appAdmin);
 
 const port = process.env.PORT || 3000;
 
 const httpsServer = https.createServer(credentials, app);
+const httpsServerAdmin = https.createServer(credentials, appAdmin);
 
 httpsServer.listen(port, () =>
 {
     console.log(`Alpha Multi Server is running at port ${port}`);
+});
+
+httpsServerAdmin.listen(3334, () =>
+{
+    console.log(`Alpha Multi Server Admin is running at port ${3334}`);
 });
 
 websocketServer.init();
