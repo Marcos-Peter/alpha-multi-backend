8d66a1dde9563c33cfb9faf213f1fcf1dadeb65f
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-09-27 23:04:21 -0400

    https

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
 import { routes } from './routes/routesList';
 import { websocketServer } from './websocket/websocketServer';
 
 dotenv.config();
 
 const app = express();
+
+const privateKey  = fs.readFileSync('certificate.key', 'utf8');
+const certificate = fs.readFileSync('certificate.crt', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
@@ -18,7 +25,9 @@ routes.initRoutes(app);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () =>
 {
     console.log(`Alpha Multi Server is running at port ${port}`);
 });
